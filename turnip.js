f7bae8cdf79cf7b51d4c4cd7cd86f2d593bec477
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-04-02 12:01:48 -0700

    Full height tiles and scroll to next

--- turnip.js
+++ turnip.js
@@ -1,5 +1,34 @@
-(function() {
+$(function() {
 	var turnip = {};
 
+	var initNextButton = function() {
+		var $window = $(window);
+		var windowHeight = $window.height();
+
+		$('.js-next-tile').on('click', function() {
+			var $currentTile;
+
+			$('.c-tile').each(function(index, el) {
+				var top = el.getBoundingClientRect().top;
+
+				// Find any tiles that are currently visible
+				if (top < windowHeight && top >= -windowHeight/2) {
+					// Save only the first visible tile
+					if (!$currentTile) {
+						$currentTile = $(el);
+					}
+				}
+			});
+
+			if ($currentTile.next().length) {
+				$('html, body').animate({
+		          scrollTop: $currentTile.next().offset().top
+		        }, 500);
+			}
+		});
+	};
+
+	initNextButton();
+
 	window.turnip = turnip;
-})();
+});
